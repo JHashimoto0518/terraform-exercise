0a36ba1cf3452bd41d0b4e138e871717308580ad
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2024-01-19 06:16:33 +0900

    add a tag to the bucket

--- bulk-import-with-block/main.tf
+++ bulk-import-with-block/main.tf
@@ -20,15 +20,8 @@ resource "aws_s3_bucket" "test-1" {
 
 resource "aws_s3_bucket" "test-2" {
   bucket = "tf-bulk-import-test-2"
-}
-
-moved {
-  from = aws_s3_bucket.this["tf-bulk-import-test-1"]
-  to   = aws_s3_bucket.test-1
-}
-
-moved {
-  from = aws_s3_bucket.this["tf-bulk-import-test-2"]
-  to   = aws_s3_bucket.test-2
+  tags = {
+    "versioning" = "true"
+  }
 }
 
