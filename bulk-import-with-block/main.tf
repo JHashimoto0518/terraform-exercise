b45b50def572101e822cebe8bb6d849261369abb
Junichi Hashimoto <46337314+JHashimoto0518@users.noreply.github.com>
2024-01-18 19:21:44 +0900

    remove: import block

--- bulk-import-with-block/main.tf
+++ bulk-import-with-block/main.tf
@@ -19,12 +19,6 @@ variable "buckets" {
   description = "buckets to import"
 }
 
-import {
-  for_each = { for b in var.buckets : b => b }
-  to       = aws_s3_bucket.this[each.key]
-  id       = each.value
-}
-
 resource "aws_s3_bucket" "this" {
   for_each = { for b in var.buckets : b => b }
   bucket   = each.value
